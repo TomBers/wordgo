59f0a94aefbf3b099bc1c7227df4b64692f7cdd3
TomBers <tjcberman@gmail.com>
2025-09-15 13:23:20 +0100

    bake model

---
+++ lib/wordgo/release.ex
@@ -0,0 +1,31 @@
+defmodule Wordgo.Release do
+  @moduledoc """
+  Used for executing DB release tasks when run in production without Mix
+  installed.
+  """
+  @app :wordgo
+
+  def migrate do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    # Many platforms require SSL when connecting to the database
+    # Load application configuration without starting supervision tree.
+    Application.ensure_all_started(:ssl)
+    Application.load(@app)
+  end
+end
